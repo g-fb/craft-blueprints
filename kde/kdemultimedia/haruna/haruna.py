5f9b93a09d8eb7c4694ddae34d5030645c819d11
g-fb <50304471+g-fb@users.noreply.github.com>
2021-12-29 18:29:38 +0200

    Update haruna.py

--- kde/kdemultimedia/haruna/haruna.py
+++ kde/kdemultimedia/haruna/haruna.py
@@ -4,6 +4,8 @@ from CraftOS.osutils import OsUtils
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
+        self.displayName = "Haruna"
+        self.description = "Haruna video player"
         self.svnTargets["master"] = "https://invent.kde.org/multimedia/haruna.git"
                 
         for ver in ["0.7.3"]:
@@ -12,9 +14,12 @@ class subinfo( info.infoclass ):
             self.archiveNames[ver] = f"haruna-v{ver}.tar.gz"
             
         self.targetDigests["0.7.3"] = (["8ef599a6b986fdff85067d9c9c47aa8d70f07e365446036247b8da1237d75bd4"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "0.7.3"
-        self.displayName = "Haruna"
-        self.description = "Haruna video player"
+
+        # patch needed for mpv master, mpv 0.34.0 doesn't have meson
+        self.patchToApply["0.7.3"] = [("0001-remove-mpv_opengl_init_params_extra_exts-field.patch", 1)]
+        self.patchToApply["master"] = [("0001-remove-mpv_opengl_init_params_extra_exts-field.patch", 1)]
+
+        self.defaultTarget = "master"
 
     def setDependencies( self ):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
@@ -43,7 +48,8 @@ class Package( CMakePackageBase ):
         CMakePackageBase.__init__(self)
 
     def createPackage(self):
-        self.defines["executable"] = "bin\\haruna.exe"
+        # TODO add icons
+        self.defines["executable"] = "src\\haruna.exe"
 
         self.defines["mimetypes"] = ["video/mkv", "video/mp4", "video/ogm", "video/avi"]
         self.defines["file_types"] = [".mkv", ".mp4", ".ogm", ".avi"]
@@ -52,3 +58,12 @@ class Package( CMakePackageBase ):
         self.ignoredPackages.append("libs/dbus")
 
         return TypePackager.createPackage(self)
+
+    def preArchive(self):
+        # can't find library if it starts with lib
+        utils.copyFile(os.path.join(self.archiveDir(), "bin", "org", "kde", "qqc2desktopstyle", "private", "libqqc2desktopstyleplugin.dll"),
+                       os.path.join(self.archiveDir(), "bin", "org", "kde", "qqc2desktopstyle", "private", "qqc2desktopstyleplugin.dll"))
+        utils.copyFile(os.path.join(self.archiveDir(), "bin", "org", "kde", "sonnet", "libsonnetquickplugin.dll"),
+                       os.path.join(self.archiveDir(), "bin", "org", "kde", "sonnet", "sonnetquickplugin.dll"))
+
+        return super().preArchive()
